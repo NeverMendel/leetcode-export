63ef23d863059a93d06b2157adbc1773edd24a83
Davide Cazzin <28535750+NeverMendel@users.noreply.github.com>
2022-01-16 12:32:31 +0100

    Change email to author_email in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url='https://github.com/NeverMendel/leetcode-export',
     license='MIT',
     author='Davide Cazzin',
-    email='cazzindavide@gmail.com',
+    author_email='cazzindavide@gmail.com',
     description='Python script to export your LeetCode solutions',
     long_description=long_description,
     long_description_content_type='text/markdown',
